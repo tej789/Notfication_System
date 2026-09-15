dfca47d742e75364c3f7ba5afd00703e2f6316e6
tej789 <gotitej2005@gmail.com>
2026-09-15 14:32:48 +0530

    Initial Commit

---
+++ src/test/java/Notification_System/NotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Notification_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
